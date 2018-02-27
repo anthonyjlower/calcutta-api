3725471d54f06c6821bd03a200d41abf8923f22c
Anthony <anthonyjlower@gmail.com>
2018-02-26 22:33:32 -0600

    Set up team model and controller to get all teams in db

--- config.ru
+++ config.ru
@@ -4,11 +4,13 @@ require 'sinatra/activerecord'
 
 # Controllers
 require './controllers/ApplicationController'
+require './controllers/TeamController'
 
 
 # Models
-
+require './models/TeamModel'
 
 
 # Mapping
 map('/'){run ApplicationController}
+map('/teams'){run TeamController}
---
+++ controllers/TeamController.rb
@@ -0,0 +1,17 @@
+class TeamController < ApplicationController
+
+	get	'/' do
+		@teams = Team.all
+
+		resp = {
+			status: {
+				success: true,
+				message: "These are all of the teams for the calcutta",
+				number_of_teams: @teams.length
+			},
+			teams: @teams
+		}
+		resp.to_json
+	end
+
+end
